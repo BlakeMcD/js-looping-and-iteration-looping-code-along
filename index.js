d8d4d129959805cc5f10a67008be66b374a66f46
BlakeMcD <blakemcd@gmail.com>
2021-07-19 15:29:23 +1000

    Assignment Completed again

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+function writeCards(blankArray,event) {
+    let newArray = [];
+    for (let i = 0; i < blankArray.length; i++)
+    {
+        newArray.push(`Thank you, ${blankArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray;
+
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
+
+console.log(countdown(10))
